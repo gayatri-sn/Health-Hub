c14c4fd60f306c9e1537ce5108977f3d1ab0a7b1
Gayatri Nambiar <149247228+gayatri-sn@users.noreply.github.com>
2025-10-02 04:58:52 +0530

    Milestone-0 HMS-MAD-1

---
+++ README.md
@@ -0,0 +1,3 @@
+AppDev 1 Project - IITM BS DEGREE
+
+This is a hospital management system that allows admins, doctors and patients to interact with the system based on their roles and needs.
---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+from controller.database import db
+from controller.config import Config
+from controller.models import *
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+    admin_role = Role.query.filter_by(name='admin').first()
+    if not admin_role:
+        admin_role = Role(name='admin')
+        db.session.add(admin_role)
+
+    
+    doctor_role = Role.query.filter_by(name='doctor').first()
+    if not doctor_role:
+        doctor_role = Role(name='doctor')
+        db.session.add(doctor_role)
+
+    patient_role = Role.query.filter_by(name='patient').first()
+    if not patient_role:
+        patient_role = Role(name='patient')
+        db.session.add(patient_role)
+
+    db.session.commit()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
+
